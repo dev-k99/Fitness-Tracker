e4fb0ac9aa00856e05529d6032ad8d8cc0c5c93f
dev-k99 <kwanelerh069@gmail.com>
2026-02-19 10:12:25 +0200

    "navbar fix"

--- fitness-tracker-frontend/src/components/Layout/Navbar.tsx
+++ fitness-tracker-frontend/src/components/Layout/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar: React.FC = () => {
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-brand">
-          💪 Fitness Tracker
+           Fitness Tracker
         </Link>
 
         <div className="navbar-menu">
